bowlofcereal/LyndvharRebase

Mammon to Groschen
Kingdom Come Deliverance has made many familiar with Groschen as a generic unit of currency.

Mammon is a biblical demon of greed.

Groschen it is. This also solves the problem Mammon had of sometimes being, and sometimes not being, written as a plural with or without an S.

code/modules/roguetown/roguemachine/steward.dm
@@ -67,13 +67,13 @@
 		if(!D)
 			return
 		if(SStreasury.treasury_value < D.get_import_price())
-			say("Insufficient mammon.")
+			say("Insufficient groschen.")
 			return
 		var/amt = D.get_import_price()
 		SStreasury.treasury_value -= amt
 		SStreasury.log_to_steward("-[amt] imported [D.name]")
 		if(amt >= 100) //Only announce big spending.
-			scom_announce("Sundmark imports [D.name] for [amt] mammon.", )
+			scom_announce("Sundmark imports [D.name] for [amt] groschen.", )
 		D.raise_demand()
 		addtimer(CALLBACK(src, PROC_REF(do_import), D.type), 10 SECONDS)
 	if(href_list["export"])
@@ -96,7 +96,7 @@
 		SStreasury.treasury_value += amt
 		SStreasury.log_to_steward("+[amt] exported [D.name]")
 		if(amt >= 100) //Only announce big spending.
-			scom_announce("Sundmark exports [D.name] for [amt] mammon.")
+			scom_announce("Sundmark exports [D.name] for [amt] groschen.")
 		D.lower_demand()
 	if(href_list["togglewithdraw"])
 		var/datum/roguestock/D = locate(href_list["togglewithdraw"]) in SStreasury.stockpile_datums

snowysquirrel/Helmsguard

code/modules/roguetown/roguemachine/steward/steward.dm
@@ -71,14 +71,14 @@
 		if(!D)
 			return
 		if(SStreasury.treasury_value < D.get_import_price())
-			say("Insufficient mammon.")
+			say("Insufficient groschen.")
 			return
 		var/amt = D.get_import_price()
 		SStreasury.treasury_value -= amt
 		SStreasury.total_import += amt
 		SStreasury.log_to_steward("-[amt] imported [D.name]")
 		if(amt >= 100) //Only announce big spending.
-			scom_announce("Sundmark imports [D.name] for [amt] mammon.", )
+			scom_announce("Sundmark imports [D.name] for [amt] groschen.", )
 		D.raise_demand()
 		addtimer(CALLBACK(src, PROC_REF(do_import), D.type), 10 SECONDS)
 	if(href_list["export"])
@@ -101,7 +101,7 @@
 		SStreasury.treasury_value += amt
 		SStreasury.log_to_steward("+[amt] exported [D.name]")
 		if(amt >= 100) //Only announce big spending.
-			scom_announce("Helmsguard exports [D.name] for [amt] mammon.")
+			scom_announce("Helmsguard exports [D.name] for [amt] groschen.")
 		D.lower_demand()
 	if(href_list["togglewithdraw"])
 		var/datum/roguestock/D = locate(href_list["togglewithdraw"]) in SStreasury.stockpile_datums