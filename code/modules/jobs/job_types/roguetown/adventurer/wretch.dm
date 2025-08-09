zeratino/Scarlet-Reach

Ports over wretches being able to be supernatural monsters and it's fixes
we love being evil

code/modules/jobs/job_types/roguetown/adventurer/wretch.dm
@@ -37,8 +37,65 @@
 		if(GLOB.adventurer_hugbox_duration)
 			addtimer(CALLBACK(H, TYPE_PROC_REF(/mob/living/carbon/human, adv_hugboxing_start)), 1)
 
-// Proc for wretch to select a bounty
+// Proc for wretch to select a bounty and supernatural affliction
 /proc/wretch_select_bounty(mob/living/carbon/human/H)
+	// First select supernatural affliction
+	var/affliction = input(H, "What supernatural curse afflicts you? (\"None\" grants +1 to all stats, \"Vampire\" and \"Werewolf\" give -1 to all stats)", "Supernatural Affliction") as null|anything in list("None (+1 to all stats)", "Vampire (-1 to all stats)", "Werewolf (-1 to all stats)")
+	
+	switch(affliction)
+		if("Vampire (-1 to all stats)")
+			var/datum/antagonist/vampire/lesser/antag = H.mind.add_antag_datum(/datum/antagonist/vampire/lesser)
+			if(antag)
+				var/mob/living/carbon/human/Hh = H
+				if(istype(Hh))
+					antag.cache_skin = Hh.skin_tone
+					antag.cache_eyes = Hh.eye_color
+					antag.cache_hair = Hh.hair_color
+				antag.wretch_antag = TRUE
+				antag.vitae = 5000 // Ensure vitae pool is set for wretch vampires
+				antag.owner = H.mind
+				H.verbs |= /mob/living/carbon/human/proc/disguise_button
+				H.verbs |= /mob/living/carbon/human/proc/vamp_regenerate
+				to_chat(H, span_danger("The thirst for blood burns within you, but you are merely one of many cursed with vampirism."))
+				// Add all vampire traits to wretch vampires
+				ADD_TRAIT(H, TRAIT_STRONGBITE, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_NOHUNGER, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_NOBREATH, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_NOPAIN, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_NOROGSTAM, TRAIT_GENERIC)
+				// Apply -1 to all stats
+				H.change_stat("strength", -1)
+				H.change_stat("perception", -1)
+				H.change_stat("intelligence", -1)
+				H.change_stat("constitution", -1)
+				H.change_stat("endurance", -1)
+				H.change_stat("speed", -1)
+				H.change_stat("fortune", -1)
+		if("Werewolf (-1 to all stats)")
+			var/datum/antagonist/werewolf/lesser/antag = H.mind.add_antag_datum(/datum/antagonist/werewolf/lesser)
+			if(antag)
+				antag.wretch_antag = TRUE
+				antag.owner = H.mind
+				to_chat(H, span_danger("The beast within yearns to be free. Your lycanthropic curse has made you a danger to all."))
+				// Apply -1 to all stats
+				H.change_stat("strength", -1)
+				H.change_stat("perception", -1)
+				H.change_stat("intelligence", -1)
+				H.change_stat("constitution", -1)
+				H.change_stat("endurance", -1)
+				H.change_stat("speed", -1)
+				H.change_stat("fortune", -1)
+		if("None (+1 to all stats)")
+			// Apply +1 to all stats
+			H.change_stat("strength", 1)
+			H.change_stat("perception", 1)
+			H.change_stat("intelligence", 1)
+			H.change_stat("constitution", 1)
+			H.change_stat("endurance", 1)
+			H.change_stat("speed", 1)
+			H.change_stat("fortune", 1)
+	
+	// Then proceed with normal bounty selection
 	var/bounty_poster = input(H, "Who placed a bounty on you?", "Bounty Poster") as anything in list("The Justiciary of Blackmoor", "The Grenzelhoftian Holy See", "The Otavan Holy See")
 	if(bounty_poster == "The Justiciary of Blackmoor")
 		GLOB.outlawed_players += H.real_name
@@ -52,8 +109,12 @@
 			bounty_total = rand(130, 200)
 		if("Harm towards lyfe")
 			bounty_total = rand(200, 350)
+			H.change_stat("intelligence", 1)
 		if("Horrific atrocities")
 			bounty_total = rand(350, 500) // Let's not make it TOO profitable
+			H.change_stat("endurance", 1)
+			H.change_stat("constitution", 1)
+			H.change_stat("intelligence", 1)
 	var/my_crime = input(H, "What is your crime?", "Crime") as text|null
 	if (!my_crime)
 		my_crime = "crimes against the Crown"

snowysquirrel/Helmsguard

code/modules/jobs/job_types/roguetown/adventurer/wretch.dm
@@ -42,8 +42,65 @@
 		if(GLOB.adventurer_hugbox_duration)
 			addtimer(CALLBACK(H, TYPE_PROC_REF(/mob/living/carbon/human, adv_hugboxing_start)), 1)
 
-// Proc for wretch to select a bounty
+// Proc for wretch to select a bounty and supernatural affliction
 /proc/wretch_select_bounty(mob/living/carbon/human/H)
+	// First select supernatural affliction
+	var/affliction = input(H, "What supernatural curse afflicts you? (\"None\" grants +1 to all stats, \"Vampire\" and \"Werewolf\" give -1 to all stats)", "Supernatural Affliction") as null|anything in list("None (+1 to all stats)", "Vampire (-1 to all stats)", "Werewolf (-1 to all stats)")
+	
+	switch(affliction)
+		if("Vampire (-1 to all stats)")
+			var/datum/antagonist/vampire/lesser/antag = H.mind.add_antag_datum(/datum/antagonist/vampire/lesser)
+			if(antag)
+				var/mob/living/carbon/human/Hh = H
+				if(istype(Hh))
+					antag.cache_skin = Hh.skin_tone
+					antag.cache_eyes = Hh.eye_color
+					antag.cache_hair = Hh.hair_color
+				antag.wretch_antag = TRUE
+				antag.vitae = 5000 // Ensure vitae pool is set for wretch vampires
+				antag.owner = H.mind
+				H.verbs |= /mob/living/carbon/human/proc/disguise_button
+				H.verbs |= /mob/living/carbon/human/proc/vamp_regenerate
+				to_chat(H, span_danger("The thirst for blood burns within you, but you are merely one of many cursed with vampirism."))
+				// Add all vampire traits to wretch vampires
+				ADD_TRAIT(H, TRAIT_STRONGBITE, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_NOHUNGER, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_NOBREATH, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_NOPAIN, TRAIT_GENERIC)
+				ADD_TRAIT(H, TRAIT_INFINITE_ENERGY, TRAIT_GENERIC)
+				// Apply -1 to all stats
+				H.change_stat("strength", -1)
+				H.change_stat("perception", -1)
+				H.change_stat("intelligence", -1)
+				H.change_stat("constitution", -1)
+				H.change_stat("endurance", -1)
+				H.change_stat("speed", -1)
+				H.change_stat("fortune", -1)
+		if("Werewolf (-1 to all stats)")
+			var/datum/antagonist/werewolf/lesser/antag = H.mind.add_antag_datum(/datum/antagonist/werewolf/lesser)
+			if(antag)
+				antag.wretch_antag = TRUE
+				antag.owner = H.mind
+				to_chat(H, span_danger("The beast within yearns to be free. Your lycanthropic curse has made you a danger to all."))
+				// Apply -1 to all stats
+				H.change_stat("strength", -1)
+				H.change_stat("perception", -1)
+				H.change_stat("intelligence", -1)
+				H.change_stat("constitution", -1)
+				H.change_stat("endurance", -1)
+				H.change_stat("speed", -1)
+				H.change_stat("fortune", -1)
+		if("None (+1 to all stats)")
+			// Apply +1 to all stats
+			H.change_stat("strength", 1)
+			H.change_stat("perception", 1)
+			H.change_stat("intelligence", 1)
+			H.change_stat("constitution", 1)
+			H.change_stat("endurance", 1)
+			H.change_stat("speed", 1)
+			H.change_stat("fortune", 1)
+	
+	// Then proceed with normal bounty selection
 	var/bounty_poster = input(H, "Who placed a bounty on you?", "Bounty Poster") as anything in list("The Justiciary of Helmsguard", "The Grenzelhoftian Holy See", "The Otavan Holy See")
 	if(bounty_poster == "The Justiciary of Helmsguard")
 		GLOB.outlawed_players += H.real_name
@@ -56,9 +113,13 @@
 		if("Misdeed")
 			bounty_total = rand(100, 200)
 		if("Harm towards lyfe")
-			bounty_total = rand(200, 300)
+			bounty_total = rand(200, 350)
+			H.change_stat("intelligence", 1)
 		if("Horrific atrocities")
-			bounty_total = rand(300, 400) // Let's not make it TOO profitable
+			bounty_total = rand(350, 500) // Let's not make it TOO profitable
+			H.change_stat("endurance", 1)
+			H.change_stat("constitution", 1)
+			H.change_stat("intelligence", 1)
 	var/my_crime = input(H, "What is your crime?", "Crime") as text|null
 	if (!my_crime)
 		my_crime = "crimes against the Province"